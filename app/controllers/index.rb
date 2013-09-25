9fbc85ce4f14dd2ea0907ddc4340b98cd9dc6a76
Team1 <student@devbootcamp.com>
2013-09-25 18:02:47 -0500

    MVP

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,7 +7,6 @@ get '/' do
 end
 
 get '/login' do
-
   erb :login
 end
 
@@ -35,28 +34,49 @@ get '/post/:post_id' do
 end
 
 get '/newpost' do
+  if current_user
+    @user_id = User.find(session[:id]).id 
+   erb :newpost
+  else
+    redirect '/login'
+  end
 end
 
-#7 views index, 
-# login, 
-# profile, 
-# allcoms, 
-# allposts, 
-# singlepost, 
-# newpost
+get '/logout' do
+  session.clear
+  redirect '/'
+end
 
 # POST ==========================================
 
 post '/login' do
-
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      session[:id] = @user.id
+      redirect "/profile/#{session[:id]}"
+    else
+      redirect '/login'
+    end
+  erb :profile
 end
 
 post '/post/:post_id' do
-  
+  @post_id = params[:post_id]
+  @user_id = User.find(session[:id]).id 
+  @comment_text = params[:comment_text]
+  Comment.create(post_id: @post_id, user_id: @user_id, comment_text: @comment_text)
+  redirect "/post/#{@post_id}"
 end
 
-post '/newpost' do
+post '/newpost/:user_id' do
+    @user_id = params[:user_id]
+    @post = Post.create(title: params[:title], url: params[:url], user_id: @user_id)
+  redirect '/'
 end
 
 post '/newuser' do
+  @user = User.create(params[:newuser])
+  redirect "/profile/#{@user.id}"
 end
+
+
