009cddf3ba9633b8c3db89166cbb4126bb39e153
Team1 <student@devbootcamp.com>
2013-09-25 11:25:18 -0500

    routing

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,52 @@
+require 'bcrypt'
+
+# GET ==========================================
 get '/' do
-  # Look in app/views/index.erb
+  @allposts = Post.all
   erb :index
+end
+
+get '/login' do
+
+  erb :login
+end
+
+get '/profile/:user_id' do
+  @user = User.find(params[:user_id])
+  erb :profile
+end
+
+get '/allcomments/:user_id' do
+end
+
+get '/allposts/:user_id' do
+end
+
+get 'post/:post_id' do
+end
+
+get '/newpost' do
+end
+
+#7 views index, 
+# login, 
+# profile, 
+# allcoms, 
+# allposts, 
+# singlepost, 
+# newpost
+
+# POST ==========================================
+
+post '/login' do
+end
+
+post '/post/:post_id' do
+  #passing in comment
+end
+
+post '/newpost' do
+end
+
+post '/newuser' do
 end
---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  include BCrypt
+
+
+  has_many :posts
+  has_many :comments
+  validates :email, uniqueness: true
+  
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+
+end
+
